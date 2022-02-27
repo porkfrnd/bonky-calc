ba39b4060dc2d61759e98b16605fc1d56edabefe
porkfrnd <90595891+porkfrnd@users.noreply.github.com>
2022-02-27 19:20:21 +0545

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,17 @@
+sus = "the answer is "
+print("made by bonky")
+first_number = float(input("first number: "))
+expression = input("+|-|*|/= ")
+second_number = float(input("second number: "))
+if expression == "+";
+ answer = first_number + second_number
+ print(sus + answer)
+elif expression == "-";
+ answer = first_number - second_numebr
+ print(sus + answer)
+elif expression == "*";
+ answer = first_number * second_number
+ print(sus + answer)
+elif expression == "/";
+ answer = first_number / second_number
+ print(sus + answer)
